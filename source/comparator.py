a3dd8cbe6875d8456ceb858c6478e8b94518b1af
ana <expectopatr12@gmail.com>
2022-05-11 19:40:04 +0200

    Finished function for comparing SAM files of simulator and tool, graphical representation of results with matplotlib

--- source/comparator.py
+++ source/comparator.py
@@ -1,4 +1,5 @@
-
+import matplotlib.pyplot as plt
+import numpy as np
 import re
 from alignment import parse_SAM_header
 
@@ -15,8 +16,54 @@ def calculate_mapping_quality_distribution(mapq_dict, mapq, align_type):
         mapq_dict[align_type]['40-50'] += 1
     else:
         mapq_dict[align_type]['50-60'] += 1 
-    
 
+    
+def get_mapq_distribution_bar(mapq_distribution_dict, aligned_reads, misaligned_reads, unmapped_reads):
+    
+    bwa_mem_aligned = [round(num/(aligned_reads+misaligned_reads)*100) for num in mapq_distribution_dict['aligned'].values()]
+    bwa_mem_misaligned = [round(num/(misaligned_reads+aligned_reads)*100) for num in mapq_distribution_dict['misaligned'].values()]
+    bottoms_bwa_aligned = []
+    bottoms_bwa_misaligned = []
+    for i in range(len(bwa_mem_aligned)):
+        item1=bwa_mem_aligned[i]
+        item2=bwa_mem_misaligned[i]
+        if item1 < item2:
+            bottoms_bwa_misaligned.append(item1)
+            bwa_mem_misaligned[i] -= item1
+            bottoms_bwa_aligned.append(0)
+        elif item1 > item2:
+            bottoms_bwa_aligned.append(item2)
+            bwa_mem_aligned[i] -= item2
+            bottoms_bwa_misaligned.append(0)
+        else:
+            bottoms_bwa_aligned.append(0)
+            bottoms_bwa_misaligned.append(0) 
+    labels = ['0-10', '10-20', '20-30', '30-40', '40-50', '50-60'] # labels on x axis
+    label_loc = np.arange(len(labels)) # positions of labels on x axis, equidistance
+    width = 0.2 # width of the bar
+    fig, ax = plt.subplots() # creates figure, plot and axis
+    bwa_mem_aligned_rects = ax.bar(label_loc - width/2, bwa_mem_aligned, width=0.1, color="green", label = "BWA MEM aligned", bottom=bottoms_bwa_aligned) # draws bars on plot
+    bwa_mem_misaligned_rects = ax.bar(label_loc - width/2, bwa_mem_misaligned, width=0.1, color="green", label = "BWA MEM misaligned", alpha=0.3, bottom=bottoms_bwa_misaligned)
+    ax.set_xlabel("Mapping quality")
+    ax.set_ylabel("Number of reads")
+    ax.set_title("Mapping quality distribution of aligned and misaligned reads")
+    ax.set_xticklabels(labels)
+    ax.set_xticks(label_loc)
+    #ax.bar_label(bwa_mem_aligned_rects, padding=3)
+    #ax.bar_label(bwa_mem_misaligned_rects, padding=3)
+    ax.legend()
+    fig.tight_layout()
+    plt.show()
+
+def get_percision_bar():
+    return
+def get_recall_bar():
+    return
+def get_AUC_func():
+    return
+def get_fscore_func():
+    return        
+           
 
 def compare(simulator_sam_file, tool_sam_file):
     unmapped_reads = 0
@@ -41,19 +88,33 @@ def compare(simulator_sam_file, tool_sam_file):
             elif sim_sam_alignment_pos != tool_sam_alignment_pos:
                 misaligned_reads += 1
                 calculate_mapping_quality_distribution(mapq_distribution_dict, tool_sam_alignment_mapq, 'misaligned')
-
             else:
                 aligned_reads += 1
                 calculate_mapping_quality_distribution(mapq_distribution_dict, tool_sam_alignment_mapq, 'aligned')
+        
+        return(mapq_distribution_dict, aligned_reads, misaligned_reads, unmapped_reads)
             
-        print("misaligned: ", misaligned_reads)
-        print("correctly aligned: ", aligned_reads)
-        print("Unmapped reads: ", unmapped_reads)
-        print(mapq_distribution_dict.items())
-            
-           
+        #print("misaligned: ", misaligned_reads)
+        #print("correctly aligned: ", aligned_reads)
+        #print("Unmapped reads: ", unmapped_reads)
+        #print(mapq_distribution_dict.items())
+
+def compare_(sim_sam_path, bwa_sam_path, bowtie_sam_path):
+    mapq_distribution_dict_bwa, aligned_bwa, misaligned_bwa, unmapped_bwa = compare(sim_sam_path, bwa_sam_path)
+    #mapq_distribution_dict_bowtie, aligned_bowtie, misaligned_bowtie, unmapped_bowtie = compare(sim_sam_path, bowtie_sam_path)
+
+    # Data visualization, graphical representation
+    get_mapq_distribution_bar(mapq_distribution_dict_bwa, aligned_bwa, misaligned_bwa, unmapped_bwa)
+    #get_mapq_distribution_bar(mapq_distribution_dict_bowtie, aligned_bowtie, misaligned_bowtie, unmapped_bowtie)
+    #get_percision_bar()
+    #get_recall_bar()
+    #get_AUC_func()
+    #get_fscore_func()
+
+    
 
 simulator_sam_file = "./out_sam/ls_orchid.sam"
-tool_sam_file = "./bwa_sam/ls_orchid_cgc.sam"
+bwa_sam_file = "./bwa_sam/ls_orchid_cgc.sam"
+bowtie_sam_file = "./bwa_sam/ls_orchid_cgc.sam"
 
-compare(simulator_sam_file, tool_sam_file)
+compare_(simulator_sam_file, bwa_sam_file, bowtie_sam_file)
